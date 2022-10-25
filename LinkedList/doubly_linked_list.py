b6cf14f7834ce6965f391a8211b04c28b0efa416
emmyvera <emmyvera01@gmail.com>
2022-10-25 22:35:04 +0100

    Updated LinkedList Added DoublyLinnkedList

---
+++ LinkedList/doubly_linked_list.py
@@ -0,0 +1,99 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
+        self.prev_node = None
+    
+    def __repr__(self):
+        return str(self.data)
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.num_of_node = 0
+    
+    def insert(self, data):
+
+        new_node = Node(data)
+        
+        if self.head is None:            
+            self.num_of_node += 1
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.num_of_node += 1
+            new_node.prev_node = self.tail
+            self.tail.next_node = new_node
+            self.tail = new_node
+        
+    def remove(self, data):
+        # No data dey this list
+        if self.head is None:
+            return
+        
+        current_node = self.head
+        
+        # Track both previous and next node
+        prev_node = None
+        next_node = None
+
+        while current_node is not None and current_node.data != data:
+            prev_node = current_node
+            #next_node = current_node.next_node
+            current_node = current_node.next_node
+        
+        # These data no dey the list
+        if current_node is None:
+            return
+        
+        # Na the head we get remove
+        if prev_node is None:
+            self.num_of_node -= 1
+            self.head = current_node.next_node
+            current_node.prev_node = None
+        else:
+            self.num_of_node -= 1
+            prev_node.next_node = current_node.next_node
+            current_node.next_node.prev_node = prev_node
+        
+
+
+    def forward_traverse(self):
+        current_node = self.head
+
+        while current_node is not None:
+            print(current_node)
+            current_node = current_node.next_node
+    
+    def backward_traverse(self):
+        current_node = self.tail
+
+        while current_node is not None:
+            print(current_node)
+            current_node = current_node.prev_node
+    
+    def size_of_list(self):
+        return self.num_of_node
+
+if __name__ == '__main__':
+    linked_list = DoublyLinkedList()
+    linked_list.insert(10)
+    linked_list.insert('Emmy')
+    linked_list.insert('Vera')
+    linked_list.insert(20)
+    linked_list.insert(21)
+    
+    linked_list.forward_traverse()
+    print('*'*10)
+    linked_list.backward_traverse()
+    
+    linked_list.remove('Vera')
+
+    print('*'*10)
+    linked_list.forward_traverse()
+    print('*'*10)
+    linked_list.backward_traverse()
+
--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -45,6 +45,36 @@ class LinkedList:
             print(actual_node.data)
             actual_node = actual_node.next_node
 
+    def remove(self, data):
+
+        # Well the list is empty
+        if self.head is None:
+            return
+
+        current_node = self.head
+
+        # We need to track the previous node 
+        # So when we get the node we replace the Ref
+        previous_node = None
+
+        while current_node is not None and current_node.data != data:
+            previous_node = current_node
+            current_node = current_node.next_node
+        
+        # He no dey the list
+        if current_node is None:
+            return
+
+        # Omo na the head we won remove O(1) running time
+        if previous_node is None:
+            self.num_of_node -= 1
+            self.head = current_node.next_node
+
+        else:
+            # Remove and replace the next node ref
+            self.num_of_node -= 1
+            previous_node.next_node = current_node.next_node
+        
 
     def size_of_list(self):
         return self.num_of_node
@@ -55,4 +85,10 @@ if __name__ == '__main__':
     linked_list.insert_start('Emmy')
     linked_list.insert_start('Vera')
     linked_list.insert_start(20)
+    linked_list.insert_end(21)
     linked_list.traverse()
+    print(linked_list.size_of_list())
+    linked_list.remove('Emmy')
+    print('*'*10)
+    linked_list.traverse()
+    print(linked_list.size_of_list())
